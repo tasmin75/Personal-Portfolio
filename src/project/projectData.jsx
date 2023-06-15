6db57d505600273c567f62cb9a329a9b6e7cc0d0
Tasmin <tansarijkm25@gmail.com>
2023-06-15 18:05:10 +0530

    Final

--- src/project/projectData.jsx
+++ src/project/projectData.jsx
@@ -8,8 +8,8 @@ const projectData = [
         title: 'kanban board',
         description: 'A Kanban Board created with React with drag and drop functionality.Along with add, delete card and list.Features: Home page with add card and add list feature, Draggable and Droppable, Card details with activity log.',
         image: project2_1,
-        github: "https://github.com/Ankush123rai/React-Kanban_Board",
-        live: "https://kanban-board-m1fe.onrender.com/",
+        github: "https://github.com/tasmin75/Kanban-Board-Project.git",
+        live: "https://kanban-project4.netlify.app/",
         tags: ['React', 'Javascript', 'HTML', 'CSS', 'React-beautiful-dnd', 'Material-UI', 'uuid', 'Redux-toolkit']
     },
     {
@@ -17,8 +17,8 @@ const projectData = [
         title: 'Twitter Clone',
         description:'A twitter clone is built in react with login and registration features, protected routing, and appropriate form validation.Features : Sign-up,Sign-in,Add, delete tweets, Feed posts like And unlike feature.Areas Of Responsibility :-Home Page, Feed section,Add tweet ,Sign-in and Sign-up page.',
         image: project1_1,
-        github: "https://github.com/Ankush123rai/new-twitter-clone",
-        live: "https://twitter-clone2-0-fl4b.onrender.com/",
+        github: "https://github.com/tasmin75/Twitter_Clone.git",
+        live: "https://twitter-clone-website-ok09.onrender.com",
 
         tags: ['React', 'Javascript', 'CSS', 'Material-UI', 'Local-storage', ]
     },
@@ -27,8 +27,8 @@ const projectData = [
         title: 'Gym Website',
         description: 'A Gym website created with React.js with multiple pages with login register functionality alongwith proper validation.Features:- Sign in, Sign up, Home page, Aboutus, Program, Training, Pricing. Areas Of Responsibility :- Home page, Aboutus page, Training page, Program page, Pricing.',
         image: project3_1,
-        github: "https://github.com/Ankush123rai/gym-project2",
-        live: "https://gym-website1-0.netlify.app/",
+        github: "https://github.com/tasmin75/gym-website.git",
+        live: "https://oxyggen-gym.onrender.com/",
 
         tags: ['React', 'Javascript', 'CSS', 'Material-UI', 'Local-storage', ]
     },
